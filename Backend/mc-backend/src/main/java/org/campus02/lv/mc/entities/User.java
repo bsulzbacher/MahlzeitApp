85348c25ac4b8238a9277d21b24b4acd2be5f2b7
Maximilian Brunnsteiner <m.brunnsteiner@edu.campus02.at>
2017-05-20 14:27:23 +0200

    Korrektur Friends

--- Backend/mc-backend/src/main/java/org/campus02/lv/mc/entities/User.java
+++ Backend/mc-backend/src/main/java/org/campus02/lv/mc/entities/User.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
 import lombok.Data;
@@ -23,7 +24,7 @@ public class User {
 	private String surname;
 	private String prename;
 	private String pwd;
-	@OneToMany
+	@ManyToMany
 	private Set<User> favorites;
 	@ManyToMany
 	@JoinTable(name="user_groups")
